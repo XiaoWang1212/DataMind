4b19e962c77d8ed6cf4351cfb45ac880242bba97
Vonni <yvonne20050822@icloud.com>
2026-08-12 21:47:55 +0800

    style(paper): apply design system to the editor and paper content typography

--- frontend/src/components/paper/StrikethroughIcon.vue
+++ frontend/src/components/paper/StrikethroughIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="strikethrough-icon" aria-hidden="true">S</span>
+  <span aria-hidden="true" class="strikethrough-icon">S</span>
 </template>
 
 <style scoped>
@@ -10,7 +10,7 @@
   width: 1em;
   height: 1em;
   font-size: 19px;
-  font-weight: 700;
+  font-weight: 500;
   line-height: 1;
   text-decoration: line-through;
   text-decoration-thickness: 1.5px;
